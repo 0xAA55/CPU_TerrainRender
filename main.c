8875b5d45d126a688fab8276470122cdab1fc475
0xAA55 <weixiaobao213@qq.com>
2021-09-28 00:49:49 +0800

    Make drawing operations directly on HDC pixel buffer.

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include<stdlib.h>
 
 // 这个窗口里面咱想要带进去的数据
-typedef struct RendererData_struct
+typedef struct AppData_struct
 {
 	// 窗口句柄
 	HWND hWnd;
@@ -20,31 +20,25 @@ typedef struct RendererData_struct
 	uint32_t BBWidth;
 	uint32_t BBHeight;
 
-	// 每一行像素的字节数和所有像素的字节数
-	size_t BBPitch;
-	size_t BBSize;
-
 	// 离屏缓冲区像素数据指针
 	uint32_t *BBPixelData;
-	uint32_t **BBPixelRowPtr;
 
 	CPUCan_p CPUCan;
 	Terrain_p Terrain;
-}RendererData_t, * RendererData_p;
+}AppData_t, * AppData_p;
 
-static RendererData_p CreateRendererData(HWND hWnd);
-static void DeleteRendererData(RendererData_p RD);
+static AppData_p CreateAppData(HWND hWnd);
+static void DeleteAppData(AppData_p RD);
 static void DoRender(HWND hWnd);
 static LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wp, LPARAM lp);
 
 // 初始化渲染器信息
-static RendererData_p CreateRendererData(HWND hWnd)
+static AppData_p CreateAppData(HWND hWnd)
 {
-	size_t i;
 	HDC hDCTemp = NULL;
 	HBITMAP DIB = NULL;
 	RECT rc;
-	RendererData_p RD = NULL;
+	AppData_p RD = NULL;
 	char StrBuf[1024];
 	struct
 	{
@@ -66,8 +60,6 @@ static RendererData_p CreateRendererData(HWND hWnd)
 	if ((int32_t)RD->BBWidth <= 0 || (int32_t)RD->BBHeight <= 0) goto FailExit;
 
 	// 填写位图信息
-	RD->BBPitch = (size_t)RD->BBWidth * 4;
-	RD->BBSize = RD->BBPitch * RD->BBHeight;
 	memset(&BMIF32, 0, sizeof BMIF32);
 	BMIF32.BMIF.biSize = sizeof BMIF32.BMIF;
 	BMIF32.BMIF.biWidth = (LONG)RD->BBWidth;
@@ -75,7 +67,7 @@ static RendererData_p CreateRendererData(HWND hWnd)
 	BMIF32.BMIF.biPlanes = 1;
 	BMIF32.BMIF.biBitCount = 32;
 	BMIF32.BMIF.biCompression = BI_BITFIELDS;
-	BMIF32.BMIF.biSizeImage = (DWORD)RD->BBSize;
+	BMIF32.BMIF.biSizeImage = (DWORD)(RD->BBWidth * RD->BBHeight * 4);
 	BMIF32.Bitfields[0] = 0x00FF0000; // 红色部分
 	BMIF32.Bitfields[1] = 0x0000FF00; // 绿色部分
 	BMIF32.Bitfields[2] = 0x000000FF; // 蓝色部分
@@ -96,14 +88,7 @@ static RendererData_p CreateRendererData(HWND hWnd)
 	SelectObject(RD->BBDC, DIB);
 	DeleteObject(DIB); // 解引用
 
-	RD->BBPixelRowPtr = malloc(RD->BBHeight * sizeof RD->BBPixelRowPtr[0]);
-	if (!RD->BBPixelRowPtr) goto FailExit;
-	for (i = 0; i < RD->BBHeight; i++)
-	{
-		RD->BBPixelRowPtr[i] = &RD->BBPixelData[i * RD->BBWidth];
-	}
-
-	RD->CPUCan = CPUCan_Create(RD->BBWidth, RD->BBHeight);
+	RD->CPUCan = CPUCan_CreateWithRGBAFB(RD->BBWidth, RD->BBHeight, RD->BBPixelData);
 	if (!RD->CPUCan) goto FailExit;
 
 	snprintf(StrBuf, sizeof StrBuf, "maps\\%d", (rand() % 2) + 1);
@@ -112,13 +97,13 @@ static RendererData_p CreateRendererData(HWND hWnd)
 
 	return RD;
 FailExit:
-	DeleteRendererData(RD);
+	DeleteAppData(RD);
 	if (hDCTemp) ReleaseDC(hWnd, hDCTemp);
 	return NULL;
 }
 
 // 释放渲染器信息
-static void DeleteRendererData(RendererData_p RD)
+static void DeleteAppData(AppData_p RD)
 {
 	if (!RD) return;
 	Terrain_Free(RD->Terrain);
@@ -130,35 +115,30 @@ static void DeleteRendererData(RendererData_p RD)
 // 进行渲染
 static void DoRender(HWND hWnd)
 {
-	int32_t x, y, i;
-	RendererData_p RD = (RendererData_p)GetWindowLongPtr(hWnd, 0);
+	AppData_p RD = (AppData_p)GetWindowLongPtr(hWnd, 0);
 	if (!RD) return;
 
-#pragma omp parallel for
-	for (i = 0; i < (int32_t)RD->BBHeight; i++)
-	{
-		memcpy(RD->BBPixelRowPtr[i], RD->CPUCan->ColorBuf->RowPointers[i], RD->BBPitch);
-	}
+	Terrain_Render(RD->Terrain);
 }
 
 // 消息处理过程
 static LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wp, LPARAM lp)
 {
 	// 当前窗口实例数据
-	RendererData_p Inst = NULL;
+	AppData_p Inst = NULL;
 	HDC hDCPaint;
 	PAINTSTRUCT ps;
 	switch (Msg)
 	{
 	case WM_CREATE: // 创建窗口
 		// 创建新的实例数据并绑定到窗口上
-		Inst = CreateRendererData(hWnd);
+		Inst = CreateAppData(hWnd);
 		if (!Inst) return -1;
 		SetWindowLongPtr(hWnd, 0, (LONG_PTR)Inst);
 		return 0;
 
 	case WM_PAINT: // 绘制窗口
-		Inst = (RendererData_p)GetWindowLongPtr(hWnd, 0);
+		Inst = (AppData_p)GetWindowLongPtr(hWnd, 0);
 		if (!Inst) return DefWindowProc(hWnd, Msg, wp, lp);
 		hDCPaint = BeginPaint(hWnd, &ps);
 		DoRender(hWnd);
@@ -167,7 +147,7 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wp, LPARAM lp)
 		return 0;
 
 	case WM_DESTROY: // 销毁窗口
-		DeleteRendererData((RendererData_p)GetWindowLongPtr(hWnd, 0));
+		DeleteAppData((AppData_p)GetWindowLongPtr(hWnd, 0));
 		SetWindowLongPtr(hWnd, 0, (LONG_PTR)0);
 		PostQuitMessage(0);
 		return 0;
