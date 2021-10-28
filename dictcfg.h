53a2cf099c1a00d15ed5a7a06864784a27eb7dad
0xAA55 <weixiaobao213@qq.com>
2021-10-28 12:23:43 +0800

    Added dictcfg_getbool() for parsing boolean words

--- dictcfg.h
+++ dictcfg.h
@@ -5,13 +5,17 @@
 
 #include <stdio.h>
 
-void log_printf(FILE *fp, const char *Format, ...);
-
 dict_p dictcfg_load(const char *cfg_path, FILE *fp_log);
 dict_p dictcfg_section(dict_p cfg,const char *name);
 
 int dictcfg_getint(dict_p section, const char *key, int def);
 char *dictcfg_getstr(dict_p section, const char *key, char *def);
 double dictcfg_getfloat(dict_p section, const char *key, double def);
+int dictcfg_getbool(dict_p section, const char *key, int def);
+
+// NOTE
+// dictcfg_getbool() returns 1 if text is 'ok' or 'yes',
+// returns 0 if text is 'false' or 'no',
+// otherwise, it returns -1.
 
 #endif
